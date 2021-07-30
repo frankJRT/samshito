054c5502c29ca6de398fcdaea6192c96a5342efc
frankJRT <frank_jr@live.com.mx>
2021-07-29 23:59:54 -0500

    Carga inicial del proyecto

---
+++ samshito/src/main/java/com/hito/am/web/app/SamshitoApplication.java
@@ -0,0 +1,13 @@
+package com.hito.am.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamshitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamshitoApplication.class, args);
+	}
+
+}
---
+++ samshito/src/test/java/com/hito/am/web/app/SamshitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hito.am.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamshitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
